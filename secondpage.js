50cae89e42761983cd9f3fb847fa39973cf34dde
Rylan Nathan Lewis <107913116+Rylan-Lewis@users.noreply.github.com>
2023-06-22 15:33:55 +0530

    Second page (semester,branch,pastyear)

---
+++ secondpage.js
@@ -0,0 +1,83 @@
+const menu1=document.querySelectorAll('.sem');
+menu1.forEach(sem=>{
+    const select=sem.querySelector('.select');
+    const symbol=sem.querySelector('.symbol');
+    const drop=sem.querySelector('.drop');
+    const option=sem.querySelectorAll('.drop li');
+    const highlighted=sem.querySelector('.highlight');
+
+    select.addEventListener('click',()=>{
+        select.classList.toggle('select-clicked');
+        symbol.classList.toggle('symbol-rotate');
+        drop.classList.toggle('drop-open');
+    });
+
+    option.forEach(options=>{
+        options.addEventListener('click',()=>{
+            highlighted.innerText=options.innerText;
+            select.classList.remove('select-clicked');
+            symbol.classList.remove('symbol-rotate');
+            drop.classList.remove('drop-open');
+            option.forEach(options => {
+                options.classList.remove('active');
+            });
+            options.classList.add('active');
+        });
+    });
+});
+
+const menu2=document.querySelectorAll('.branch');
+menu2.forEach(branch=>{
+    const select=branch.querySelector('.select');
+    const symbol=branch.querySelector('.symbol');
+    const drop=branch.querySelector('.drop');
+    const option=branch.querySelectorAll('.drop li');
+    const highlighted=branch.querySelector('.highlight');
+
+    select.addEventListener('click',()=>{
+        select.classList.toggle('select-clicked');
+        symbol.classList.toggle('symbol-rotate');
+        drop.classList.toggle('drop-open');
+    });
+
+    option.forEach(options=>{
+        options.addEventListener('click',()=>{
+            highlighted.innerText=options.innerText;
+            select.classList.remove('select-clicked');
+            symbol.classList.remove('symbol-rotate');
+            drop.classList.remove('drop-open');
+            option.forEach(options => {
+                options.classList.remove('active');
+            });
+            options.classList.add('active');
+        });
+    });
+});
+
+const menu3=document.querySelectorAll('.pastyear');
+menu3.forEach(pastyear=>{
+    const select=pastyear.querySelector('.select');
+    const symbol=pastyear.querySelector('.symbol');
+    const drop=pastyear.querySelector('.drop');
+    const option=pastyear.querySelectorAll('.drop li');
+    const highlighted=pastyear.querySelector('.highlight');
+
+    select.addEventListener('click',()=>{
+        select.classList.toggle('select-clicked');
+        symbol.classList.toggle('symbol-rotate');
+        drop.classList.toggle('drop-open');
+    });
+
+    option.forEach(options=>{
+        options.addEventListener('click',()=>{
+            highlighted.innerText=options.innerText;
+            select.classList.remove('select-clicked');
+            symbol.classList.remove('symbol-rotate');
+            drop.classList.remove('drop-open');
+            option.forEach(options => {
+                options.classList.remove('active');
+            });
+            options.classList.add('active');
+        });
+    });
+});
